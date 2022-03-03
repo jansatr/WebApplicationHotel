bff14b77992c828ca78307374d736614db0cb6be
jansatr <Jaanus@JAANUS>
2022-03-03 21:03:02 +0200

    Add project files.

---
+++ WebApplicationHotel/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationHotel.Models
+{
+    public class UserModel
+    {
+        public string EmailAddress { get; set; }
+        public string ConfirmEmail { get; set; }
+        public string Password { get; set; }
+        public string  ConfirmPassword { get; set; }
+    }
+}
