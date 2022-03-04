a518c72b829d1437ac276f0a1940fbd71af2f9cf
Jaanus <mcsulo@hotmail.com>
2022-03-04 16:13:02 +0200

    new commit

---
+++ MVCDemoDB/dbo/Tables/Booking.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Booking]
+(
+	[BookingId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CustomerId] INT NOT NULL, 
+    [BookingFrom] DATE NOT NULL, 
+    [BookingTo] DATE NOT NULL, 
+    [AssignedRoomId] INT NOT NULL, 
+    [TotalAmount] DECIMAL NOT NULL
+)
---
+++ MVCDemoDB/dbo/Tables/Room.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Room]
+(
+	[RoomId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [RoomSize] INT NOT NULL, 
+    [RoomPrice] DECIMAL NOT NULL, 
+    [Description] VARCHAR(150) NULL
+)
